bbaf5ff82a733c87eed6b64f9f72311b9571a383
Elbie-em <emoonga1993@gmail.com>
2020-08-21 18:32:14 +0200

    Add tests for player module

--- test/player.test.js
+++ test/player.test.js
@@ -1,2 +1,13 @@
 import Player from '../src/modules/player';
 
+test('creating a user instance for player X', () => {
+    const playerOne = Player(1,'testPlayer','X');
+    expect(playerOne).toEqual({id:1,name:'testPlayer',symbol:'X'})
+    expect(playerOne).not.toEqual({id:7,name:'testPlayer',symbol:'B'});
+});
+
+test('creating a user instance for player O', () => {
+    const playerOne = Player(2,'testPlayer','O');
+    expect(playerOne).toEqual({id:2,name:'testPlayer',symbol:'O'})
+    expect(playerOne).not.toEqual({id:10,name:'testPlayer',symbol:'Y'});
+});
