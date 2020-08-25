6c649dcf25c92c95377431985c3ce38c75281ec3
Elbie-em <emoonga1993@gmail.com>
2020-08-25 22:06:04 +0200

    Add validation test for empty input when registering players

--- test/game.test.js
+++ test/game.test.js
@@ -1,1 +1,18 @@
 import * as Game from '../src/modules/game';
+
+describe('Validation checks for players names', () => {
+  test('return error message stating name cannot be empty for player one', () => {
+    const players = ['','exampleName'];
+    expect(Game.validatePlayersNames(players).message).toEqual("Name(s) cannot be empty");
+  });
+
+  test('return error msg stating name cannot be empty for player two', () => {
+    const players = ['exampleName',''];
+    expect(Game.validatePlayersNames(players).message).toEqual("Name(s) cannot be empty");
+  });
+
+  test('return error msg stating names cannot be empty for both players', () => {
+    const players = ['',''];
+    expect(Game.validatePlayersNames(players).message).toEqual("Name(s) cannot be empty");
+  });
+});
